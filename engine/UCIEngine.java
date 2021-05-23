ca292ede01a45facd16842204e260fd57943860a
Chris Parker <chrisparker060404@gmail.com>
2021-05-23 00:54:24 -0400

    Update UCIEngine.java

--- engine/UCIEngine.java
+++ engine/UCIEngine.java
@@ -1,11 +1,41 @@
 package engine;
 
+import java.io.*;
+import java.util.stream.Collectors;
+
 public class UCIEngine {
     private final String PATH;
+    private final File DIR;
+    private Process p;
+    private Runtime rt;
     
-    public UCIEngine(String path) {
+    /**
+     * Initializes the chess engine 
+     * @param path Path of the engine executable (including .exe)
+     * @throws IOException If path does not exist or cannot be accessed
+     */
+    public UCIEngine(String path) throws IOException {
         PATH = path;
+        rt = Runtime.getRuntime();
+        p = rt.exec(PATH);
+        DIR = new File(PATH);
+        BufferedReader output = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        
+        System.out.println(output.lines().collect(Collectors.joining(System.lineSeparator())));
     }
 
+    // public String exec(String command) throws IOException{
+    //      rt.exec(command, null, DIR);
+    // }
     
+    // ACCESSOR METHODS
+    public String getPath() {
+        return PATH;
+    }
+}
+
+class UCITest {
+    public static void main(String[] args) throws IOException {
+        UCIEngine engine = new UCIEngine("C:/Users/chris/Desktop/stockfish_13_win_x64_bmi2/stockfish_13");
+    }
 }
