6dc3276304a8a7c0d10eb8674004de50d84e02aa
Chris Parker <chrisparker060404@gmail.com>
2021-05-27 19:00:53 -0400

    Create classes for playing chess
    Created an outline of the program. i.e. Board, Piece, Chess, ChessGUI.

---
+++ engine/chess.py
@@ -0,0 +1,16 @@
+# By Chris Parker
+
+class Board: 
+    pass
+
+class Piece:
+    pass
+
+class Chess:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ engine/chessGUI.py
@@ -0,0 +1,13 @@
+# By Chris Parker
+
+from chess import Board, Piece, Chess
+import uciEngine
+
+class ChessGUI:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
